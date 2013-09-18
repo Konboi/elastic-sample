07152016bb6b62d3c1c19692d756f5e80db4a9e9
Konboi <ryosuke.yabuki@gmail.com>
2013-09-18 21:49:50 +0900

    sample project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TireSample::Application.config.secret_key_base = 'd0415578924b3705e978da85cc3a87f5a52a0289200f71df64d47bc6137ff20344df3b013065feb8b8916525f04f4e7141a1a7b6435bb3e131370e6bf3116879'
---
+++ db/seeds.rb
@@ -0,0 +1,53 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+data = [
+  {
+    title: "aaaa",
+    body: "aaaaa"
+  },
+  {
+    title: "bbbbbb",
+    body: "bbbbbbb"
+  },
+  {
+    title: "cccc",
+    body: "cccccccccc"
+  },
+  {
+    title: "bbbbbb",
+    body: "bbbbbb"
+  },
+  {
+    title: "eeeee",
+    body: "eeeee"
+  },
+  {
+    title: "ffffff",
+    body: "fffffff"
+  },
+  {
+    title: "aaaaa",
+    body: "search word"
+  },
+  {
+    title: "aaaaa",
+    body: "search word word"
+  },
+  {
+    title: "aaaaa",
+    body: "search word word word"
+  },
+]
+
+data.each do |d|
+  Topic.create(
+    title: d[:title],
+    body: d[:body]
+  )
+end
